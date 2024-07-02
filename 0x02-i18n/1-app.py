f333aa2b7dcf7631e02d36b0444d939cedd9d3b0
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-02 22:17:12 +0100

    Task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get_locale function"""
+    return (request.accept_languages.best_match(app.config['LANGUAGES']))
+
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+
+@app.route("/", strict_slashes=False)
+def home_page():
+    """Render template"""
+    return (render_template('0-index.html'))
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host='0.0.0.0')
---
+++ 0x02-i18n/config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Configuration file"""
+
+
+class Config:
+    """Class config for babel"""
+    LANGUAGES = ["en", "fr"]
