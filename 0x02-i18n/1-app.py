55c94c968dd76160c90599fa9c116072783e823e
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-07-03 05:56:43 +0100

    1 edit

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,11 +11,11 @@ babel = Babel(app)
 class Config:
     """Class config for babel"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 
 @app.route("/", strict_slashes=False)
